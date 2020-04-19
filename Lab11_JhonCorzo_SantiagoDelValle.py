cfa550f791b445a04e227b20725f73a6058f0ef3
SandelsCol <62896050+SandelsCol@users.noreply.github.com>
2020-04-19 18:17:27 -0500

    El comienzo de una aventura

--- Lab11_JhonCorzo_SantiagoDelValle.py
+++ Lab11_JhonCorzo_SantiagoDelValle.py
@@ -3,8 +3,15 @@ import numpy as np
 #Creacion de personaje con Stacks Aleatorios 
 #Planteamento General
 #Balanceo de estadisticas según el personaje
+#El comienzo de una aventura
 
-n=int(input("""Bienvenido a Dungeons And Dragons , para poder escoger su personaje será de la siguiente manera:
+print("""Dungeons and Dragons en un juego de mesa en el cual podrás ser aquel aventurero lleno de gloria y fama por cazar demonios
+Será un héroe pero no una leyenda ; No , hasta que no mates al rey de los dragones Sir Arthas no serás considerado así , pero esees 
+tu destino según las antiguas profecias Comenzaras como un campesino hasta llegar a ser una  leyenda¿Estas listo para el viaje?""")
+
+Name_User=str(input("Por favor digite el nombre de su personaje"))
+
+n=int(input("""Para poder escoger su personaje será de la siguiente manera:
 1= Barbaro 
 2= Bardo
 3= Clérigo
@@ -34,6 +41,7 @@ if n>=1 and n<=8:
             Barbaro_fisico= Fisicos_Fisicos[z-1][x-1]
             Barabaro_Mentales= Fisicos_Mentales[z-1][x-1]
             print("Sus atributos son", Barbaro_fisico,Barabaro_Mentales ,",Estos atributos representan Fuerza, Destreza, Constitución, Inteligencia, Sabiduría y Carisma , respectivamente")
+            print(Name_User,"Preparado para ser la leyenda y entrar al Valhalla como un legendario barbaro")
         else:
             print("Por favor haga bien el procedimiento")
 
@@ -46,6 +54,7 @@ if n>=1 and n<=8:
             Bardo_fisico= Mentales_Fisicos [z-1][x-1]
             Bardo_Mental= Mentales_Mentales [z-1][x-1]
             print("Sus atributos son",Bardo_fisico,Bardo_Mental , ",Estos atributos representan Fuerza, Destreza, Constitución, Inteligencia, Sabiduría y Carisma , respectivamente")
+            print(Name_User,"Preparado para ser aquel autor de las leyendas mas épicas y legendaria de la historia , como el mejor bardo jamas creado")
         else:
             print("Por favor haga bien el procedimiento")
 
@@ -58,6 +67,7 @@ if n>=1 and n<=8:
             clerigo_fisico= Mentales_Fisicos [z-1][x-1]
             clerigo_Mental= Mentales_Mentales [z-1][x-1]
             print("Sus atributos son",clerigo_fisico,clerigo_Mental,",Estos atributos representan Fuerza, Destreza, Constitución, Inteligencia, Sabiduría y Carisma , respectivamente")
+            print(Name_User,"Dios te hace portador de su poder , usalo bien Clérigo")
         else:
             print("Por favor haga bien el procedimiento")
 
@@ -70,6 +80,7 @@ if n>=1 and n<=8:
             Druida_fisico= Mentales_Fisicos [z-1][x-1]
             Druida_Mental= Mentales_Mentales [z-1][x-1]
             print("Sus atributos son",Druida_fisico,Druida_Mental,",Estos atributos representan Fuerza, Destreza, Constitución, Inteligencia, Sabiduría y Carisma , respectivamente")
+            print(Name_User,"El equilibrio separa la vida de la muerte , el orden del caos , debes ser aquel que lo mantenga Druida")
         else:
             print("Por favor haga bien el procedimiento")
 
@@ -82,6 +93,7 @@ if n>=1 and n<=8:
             Guerrero_fisico= Fisicos_Fisicos[z-1][x-1]
             Guerrero_mental= Fisicos_Mentales[z-1][x-1]
             print("Sus atributos son",Guerrero_fisico,Guerrero_mental,",Estos atributos representan Fuerza, Destreza, Constitución, Inteligencia, Sabiduría y Carisma , respectivamente")
+            print(Name_User,"Una muerte en la batalla , significará la gloria para nosotros , los guerreros")
         else:
             print("Por favor haga bien el procedimiento")            
 
@@ -94,6 +106,7 @@ if n>=1 and n<=8:
             Mago_fisico= Mentales_Fisicos [z-1][x-1]
             Mago_Mental= Mentales_Mentales [z-1][x-1]
             print("Sus atributos son",Mago_fisico,Mago_Mental,",Estos atributos representan Fuerza, Destreza, Constitución, Inteligencia, Sabiduría y Carisma , respectivamente")
+            print(Name_User,"El arte del conocimiento y del caos, eso somos los magos")
         else:
             print("Por favor haga bien el procedimiento")
 
@@ -106,6 +119,7 @@ if n>=1 and n<=8:
             Paladin_fisico= Fisicos_Fisicos[z-1][x-1]
             paladin_mental= Fisicos_Mentales[z-1][x-1]
             print("Sus atributos son",Paladin_fisico,paladin_mental,",Estos atributos representan Fuerza, Destreza, Constitución, Inteligencia, Sabiduría y Carisma , respectivamente")
+            print(Name_User,"La luz te bendice , debes responderlé Sir Paladin")
         else:
             print("Por favor haga bien el procedimiento")
 
@@ -118,6 +132,8 @@ if n>=1 and n<=8:
             Picaro_fisico= Fisicos_Fisicos[z-1][x-1]
             Picaro_mental= Fisicos_Mentales[z-1][x-1]
             print("Sus atributos son",Picaro_fisico,Picaro_mental,",Estos atributos representan Fuerza, Destreza, Constitución, Inteligencia, Sabiduría y Carisma , respectivamente")
+            print(Name_User,"""Si en el mundo no hubierán mas tontos que picaros, los picaros no tendrían de quien  
+            Aprovecharse para vivir""")
         else:
             print("Por favor haga bien el procedimiento")
 
